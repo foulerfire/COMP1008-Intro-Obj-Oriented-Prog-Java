e026f60dfb86165198eb2702b76cb72680562b4e
Matt Wilhelm <foulerfire@gmail.com>
2026-02-08 18:30:31 -0500

    created commission employee object, and display code for earnings

--- Labs/Lab3/PayrollTest.java
+++ Labs/Lab3/PayrollTest.java
@@ -1,11 +1,14 @@
 public class PayrollTest {
  
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
  
         // TODO 9: Create a CommissionEmployee object
- 
+        CommissionEmployee employee =  new CommissionEmployee("Matt", "Wilhelm", "123-45-6789", 5000.0, 0.10);
         // TODO 10: Display employee details
- 
+        System.out.println(employee);
         // TODO 11: Display earnings
+        System.out.println("Earnings: $" + employee.earnings());
+
     }
 }
