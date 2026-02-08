0b4305c23748092ada9f8f7ac7350966af0bd7d8
Matt Wilhelm <foulerfire@gmail.com>
2026-02-08 16:21:09 -0500

    created starting files

---
+++ Labs/Lab3/Employee.java
@@ -0,0 +1,14 @@
+public abstract class Employee {
+ 
+    private String firstName;
+    private String lastName;
+    private String socialSecurityNumber;
+ 
+    // TODO 1: Create a constructor
+ 
+    // TODO 2: Create getters
+ 
+    // TODO 3: Declare abstract method earnings()
+ 
+    // TODO 4: Override toString()
+}
---
+++ Labs/Lab3/PayrollTest.java
@@ -0,0 +1,11 @@
+public class PayrollTest {
+ 
+    public static void main(String[] args) {
+ 
+        // TODO 9: Create a CommissionEmployee object
+ 
+        // TODO 10: Display employee details
+ 
+        // TODO 11: Display earnings
+    }
+}
