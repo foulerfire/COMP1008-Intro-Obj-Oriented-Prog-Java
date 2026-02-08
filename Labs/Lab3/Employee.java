b9d756f6e72bfe63e6bab7d30da9a1f7878f92c9
Matt Wilhelm <foulerfire@gmail.com>
2026-02-08 16:27:35 -0500

    Created constructor and getters

--- Labs/Lab3/Employee.java
+++ Labs/Lab3/Employee.java
@@ -5,9 +5,27 @@ public abstract class Employee {
     private String socialSecurityNumber;
  
     // TODO 1: Create a constructor
- 
+    public Employee(String firstName, String lastName, String ssn) 
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = ssn;
+    }
     // TODO 2: Create getters
- 
+    public String getFirstName() 
+    {
+        return firstName;
+    }
+    
+    public String getLastName() 
+    {
+        return lastName;
+    }
+
+    public String getSocialSecurityNumber() 
+    {
+        return socialSecurityNumber;
+    }
     // TODO 3: Declare abstract method earnings()
  
     // TODO 4: Override toString()
