bbcd19f5e2396ff6e20766eeb6f5ea23ae8382a8
Matt Wilhelm <foulerfire@gmail.com>
2026-02-08 16:42:17 -0500

    created constructor using super and created getters and setters with validation

--- Labs/Lab3/CommissionEmployee.java
+++ Labs/Lab3/CommissionEmployee.java
@@ -4,9 +4,23 @@ public class CommissionEmployee extends Employee {
     private double commissionRate;
  
     // TODO 5: Create constructor using super()
- 
+    public CommissionEmployee(String firstName, String lastName, String ssn, double grossSales, double commissionRate) 
+    {
+        super(firstName, lastName, ssn);
+
+        if (grossSales >= 0) {
+            this.grossSales = grossSales;
+        }
+
+        if (commissionRate > 0 && commissionRate < 1) {
+            this.commissionRate = commissionRate;
+        }
+    }
     // TODO 6: Create getters and setters with validation
- 
+    public double getGrossSales() 
+    {
+        return grossSales;
+    }
     // TODO 7: Override earnings()
  
     // TODO 8: Override toString()
