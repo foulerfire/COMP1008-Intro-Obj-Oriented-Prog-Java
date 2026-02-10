28087c3ed22ebe0327ec9f6a6248dd34909a997d
Matt Wilhelm <foulerfire@gmail.com>
2026-02-10 03:08:34 -0500

    AddED default constructor to Book

--- Assignments/Assignment1/Book.java
+++ Assignments/Assignment1/Book.java
@@ -5,4 +5,11 @@ public class Book
     private String isbn;
     private boolean available;
     
+    public Book() 
+    {
+        title = "Unknown";
+        author = "Unknown";
+        isbn = "0000000000";
+        available = true;
+    }
 }
