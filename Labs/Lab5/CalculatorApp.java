a2035429d6500a194be03fb537bbcb6e3f37e8d4
Matt Wilhelm <foulerfire@gmail.com>
2026-03-29 21:47:46 -0400

    added scene and show stage

--- Labs/Lab5/CalculatorApp.java
+++ Labs/Lab5/CalculatorApp.java
@@ -92,7 +92,10 @@ public class CalculatorApp extends Application {
         });
 
         // Step 7: Create scene and show stage
-       
+        Scene scene = new Scene(mainLayout, 300, 200);
+        primaryStage.setTitle("Simple Calculator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
  
     public static void main(String[] args) {
